23ef6f7089b7b420fbdb826d92f48ff44bb2bf18
rain1024 <brother.rain.1024@gmail.com>
2016-05-09 22:52:57 +0700

    finish vv_extractor

--- script/vv_extractor.py
+++ script/vv_extractor.py
@@ -1,1 +1,21 @@
+import requests
+import json
+
 __author__ = 'LanAnh'
+import re
+
+with open("../data/VV/vv30K.dict") as text_file:
+    content = text_file.read()
+    result = re.findall("(^@.*$)", content, re.MULTILINE)
+    result = result[3:]
+    length = len(result)
+    for index, text in enumerate(result):
+        text = text[1:]
+        print "{}/{}: {}".format(index + 1, length, text)
+        url = "http://192.168.99.100:8000/api/words/"
+        data = {'name': text, 'type': 'a'}
+        headers = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        pass
